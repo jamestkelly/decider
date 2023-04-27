241c73c43b5708b5ee4c8fbae835653a7d946fb4
Jim Kelly <jimkelly.t@outlook.com>
2023-04-27 10:14:52 +1000

    feat: Added basic about page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import HomePage from "./pages/HomePage";
 import LandingPage from "./pages/LandingPage";
+import AboutPage from "./pages/AboutPage";
 
 interface Props {
     setIsAuthenticated: (value: boolean) => void;
@@ -21,6 +22,7 @@ function App() {
                 ) : (
                   <Routes>
                       <Route path={"/"} element={<HomePage/>}/>
+                    <Route path={"/about"} element={<AboutPage/>}/>
                   </Routes>
                 )}
             </Router>
